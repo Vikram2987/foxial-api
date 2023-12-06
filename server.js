7e35013d84d85179133c7130b9e80724b5379fdc
Vikram <saivikram@gmail.com>
2023-12-06 18:32:44 +0530

    final

--- server.js
+++ server.js
@@ -1,42 +1,48 @@
 const express = require('express')
 const http = require('http');
 const mongoose = require('mongoose')
+const { MongoClient } = require('mongodb');
 require("dotenv").config()
-const dbUrl = `mongodb+srv://saivikramlingampally:${process.env.MONGODB_PASSWORD}@cluster0.hmvmdw4.mongodb.net/?retryWrites=true&w=majority`
+const dbUrl = process.env.MONGODB_STRING
 const cors = require('cors')
 const app = express()
 const server = http.createServer(app);
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-const session = require('express-session');
-
-app.use(session({ secret: process.env.SESSION_KEY, resave: true, saveUninitialized: true }));
 
 
 const path = require('path');
 // Using Cors
 
 // Getting router
-const Router = require("./routes/index")
+const Router = require("./routes/index");
 
 
-//Connecting to DataBase
-mongoose.connect(dbUrl);
-const con = mongoose.connection;
 
-//On connecting to DataBase
-con.on('open',function(){
-    console.log("Connected to the db")
-})
+
+
 
 app.use('/uploads', express.static(path.join(__dirname+'/uploads')));
 // Using the router
 app.use("/",Router)
 const PORT = process.env.PORT;
 //Listening to the port
-const Server = server.listen(PORT,function(){
-    console.log("Connected to the server")
+
+const connectDB = async () => {
+    try {
+      await mongoose.connect(process.env.MONGODB_STRING);
+      console.log(`MongoDB Connected`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
+
+  connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
 })
 
 // const io = require('socket.io')(Server, {
